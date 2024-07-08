e4680f4912225a0ae073cd4f420d742cfe06a3fa
unknown <13477550937@163.com>
2024-07-08 22:04:18 +0800

    first commit

---
+++ MDK-ARM/OV_Watch_V2.3/lv_utils.d
@@ -0,0 +1,4 @@
+ov_watch_v2.3\lv_utils.o: ..\Middlewares\LVGL\GUI\lvgl\src\misc\lv_utils.c
+ov_watch_v2.3\lv_utils.o: D:\downloads\keil526\ARM\ARMCC\Bin\..\include\stddef.h
+ov_watch_v2.3\lv_utils.o: ..\Middlewares\LVGL\GUI\lvgl\src\misc\lv_utils.h
+ov_watch_v2.3\lv_utils.o: D:\downloads\keil526\ARM\ARMCC\Bin\..\include\stdint.h
